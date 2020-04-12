a7fd4dffc0584e1e53847860ce9b881623284e11
fantom1408 <52611340+fantom1408@users.noreply.github.com>
2020-04-12 22:22:45 +0300

    анимация в index

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ const cartButton = document.querySelector('#card-button');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
 
-// cartButton.addEventListener('click', function(event){
-//     modal.classList.add('is-open');
-//     });
-//     close.addEventListener('click', function(event){
-//         modal.classList.remove('is-open');
-//         });
+cartButton.addEventListener('click', function(event){
+    modal.classList.add('is-open');
+    });
+    close.addEventListener('click', function(event){
+        modal.classList.remove('is-open');
+        });
 
-        cartButton.addEventListener('click',toggleModal);
+        // cartButton.addEventListener('click',toggleModal);
            
-            close.addEventListener('click', toggleModal);
+        //     close.addEventListener('click', toggleModal);
                
 
-        function toggleModal(){
-            modal.classList.toggle('is-open');
-        }
+        // function toggleModal(){
+        //     modal.classList.toggle('is-open');
+        // }
 
         new WOW().init();
